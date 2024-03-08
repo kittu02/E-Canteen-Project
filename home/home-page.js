0978bcfaf428889a4107bb3b3ef31381c0824b45
KIRTAN <kirtan02y@gmail.com>
2024-03-08 23:59:31 +0530

    push

--- home/home-page.js
+++ home/home-page.js
@@ -1,35 +1,13 @@
-let slideIndex = 1;
-showSlides(1); // Ensure at least one slide is initially shown
+const slideContainer = document.getElementById("container2");
+const slides = slideContainer.querySelectorAll("img");
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+let currentSlideIndex = 0;
+function changeSlide() {
+  slides[currentSlideIndex].style.opacity = 0; // Fade out current slide
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length; // Cycle through slides
+  slides[currentSlideIndex].style.opacity = 1; // Fade in next slide
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  } else if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  if (dots) { 
-    dots[slideIndex - 1].className += " active";
-  }
-}
+setInterval(changeSlide, 2000);
 
 function searchdata(){
     var searchName = document.getElementById("searchTab")

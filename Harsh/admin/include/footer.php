4d7216136bee66d22ecf5daa84c5d888053e0694
KIRTAN <kirtan02y@gmail.com>
2024-05-08 10:46:10 +0530

    push

--- Harsh/admin/include/footer.php
+++ Harsh/admin/include/footer.php
@@ -1,1 +1,1 @@
- <footer class="footer" style="text-align: center;">E-Canteen - Marwadi University - <a href="https://student.marwadiuniversity.ac.in:553/">MH RONY</a> </footer>
+ <footer class="footer" style="text-align: center;">E-Canteen - <a href="https://student.marwadiuniversity.ac.in:553/"></a> Marwadi University </footer>
